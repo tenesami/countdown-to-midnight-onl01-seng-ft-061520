dc792907596823465ad161cb894b69d0a430fb16
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-06-16 03:33:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
 n = 10
 def countdown(n)
-  countdown_with_sleep(5.seconds)
+
   while n > 0
+    countdown_with_sleep(5.seconds)
     puts "#{n} SECOND(S)!"
     n -= 1
   end
