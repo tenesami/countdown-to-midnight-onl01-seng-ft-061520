9b6dcea2cebe10a800d54e3d7822ecc80f6a5fe4
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-06-16 03:34:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,11 @@
 #write your code here
 n = 10
+countdown_with_sleep(5.seconds)
 def countdown(n)
 
   while n > 0
-
     puts "#{n} SECOND(S)!"
-    countdown_with_sleep(5.seconds)
     n -= 1
-
   end
 
   return "HAPPY NEW YEAR!"
